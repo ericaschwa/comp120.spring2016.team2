099f2844fa7a10e5ec0b6bc1193b93917e830789
Erica Schwartz <Erica_S.Schwartz@tufts.edu>
2016-02-11 11:19:34 -0500

    now, form displays on submit

--- frontend/form.js
+++ frontend/form.js
@@ -1,4 +1,16 @@
- $( "form" ).on( "submit", function( event ) {
-  event.preventDefault();
-  console.log( $( this).serialize() );
+
+$( document ).ready(function() {
+	// from http://stackoverflow.com/questions/169506/obtain-form-input-fields-using-jquery
+	$( "#form" ).on( "submit", function( event ) {
+	 	event.preventDefault();
+	    var $inputs = $('#form :input');
+	    var values = {};
+	    $inputs.each(function() {
+	        if (this.name === "") {
+	        	console.log(values);
+	        } else {
+	        	values[this.name] = $(this).val();
+	        }
+	    });
+	});
 });
