0f092036ae03fd63f4d3abae28ac125f1a2d31b9
Norman Young <nyyoung24@gmail.com>
2016-02-12 03:41:11 -0500

    added initial map

--- frontend/form.js
+++ frontend/form.js
@@ -24,19 +24,19 @@ $( document ).ready(function() {
 
 });
 
-(function() {
-   window.onload = function(){
-   var latlng = new google.maps.LatLng(35.9886, -78.9072);
-   var options = {
-      zoom: 13,
-      center: latlng,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-        }; 
-  var map = new google.maps.Map(document.getElementById('map'), options);
-        }
-    })();
-
 //Google Maps JavaScript
+(function() {
+	window.onload = function(){
+   		var latlng = new google.maps.LatLng(35.9886, -78.9072);
+   		var options = {
+      		zoom: 13,
+      		center: latlng,
+      		mapTypeId: google.maps.MapTypeId.ROADMAP
+    	}; 
+  	var map = new google.maps.Map(document.getElementById('map'), options);
+    }
+})();
+
 
 /*var map;
 function initMap() {
@@ -45,74 +45,3 @@ function initMap() {
     zoom: 8
   });
 }*/
-
-/*
-var myName = "AnitaHolleman";
-var currLat;
-var currLng;
-var request = new XMLHttpRequest;
-var params;
-var data;
-var current;
-var map;
-var mymarker;
-var infowindow = new google.maps.InfoWindow();
-var mapOptions = {
-	center: current,
-	zoom: 15,
-	mapTypeId: google.maps.MapTypeId.ROADMAP
-};
-
-function init() {
-	map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
-	render();
-}
-
-function render() {
-	//find location
-	if (navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition(function(position) {
-			currLat = position.coords.latitude;
-			currLng = position.coords.longitude;
-			manageData();
-			render();
-		});
-	}
-	else {
-		alert("ERROR: Geolocation not supported");
-	}
-	//manage data
-	params = "login=" + myName + "&lat=" + currLat + "&lng=" + currLng;
-					request.open("POST", "https://sleepy-reaches-1354.herokuapp.com/sendLocation", true);
-					request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-					request.onreadystatechange = parseData;
-					request.send(params);	
-	//render
-	current = new google.maps.LatLng(currLat, currLng);
-	map.panTo(current);
-	myMarker = new google.maps.Marker({
-		position: current,
-		animation: google.maps.Animation.DROP,
-	});
-	myMarker.setMap(map);
-	google.maps.event.addListener(myMarker, "click", function() {
-		infowindow.close();
-		infowindow.setContent("<div id='login'>" + myMarker.title + "</div>");
-		infowindow.open(map, this);
-	});
-}
-
-function parseData()
-			{
-				// request is ready and status code is successful
-				if (request.readyState == 4 && request.status == 200) {
-					data = JSON.parse(request.responseText);
-					for (i = 0; i < data.length; i++) {
-						if (data[i].login == myName) continue;
-						person.name = data[i].login;
-						person.Lat = data[i].lat;
-						person.Lng = data[i].lng;
-						createMarker(person);
-					}
-				}
-			} */
