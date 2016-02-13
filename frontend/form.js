3d746162c24028dbfed3c94d091aa265cad65eb9
Norman Young <nyyoung24@gmail.com>
2016-02-13 13:37:39 -0500

    updated map

--- frontend/form.js
+++ frontend/form.js
@@ -34,14 +34,7 @@ $( document ).ready(function() {
       		mapTypeId: google.maps.MapTypeId.ROADMAP
     	}; 
   	var map = new google.maps.Map(document.getElementById('map'), options);
-    }
+  	}
+
+
 })();
-
-
-/*var map;
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
-  });
-}*/
