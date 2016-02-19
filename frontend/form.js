5fbb977d13f3e7be46a9c819e950e66efae66964
Janeth Jepkogei <janeth.jepkogei@tufts.edu>
2016-02-19 00:16:33 -0500

    added a file upload and preview button for images

--- frontend/form.js
+++ frontend/form.js
@@ -45,3 +45,21 @@ var descriptionedit = function() {
 }
 
 google.maps.event.addDomListener(window, "load", init);
+
+function previewFile(){
+       var preview = document.querySelector('img'); //selects the query named img
+       var file    = document.querySelector('input[type=file]').files[0]; //sames as here
+       var reader  = new FileReader();
+
+       reader.onloadend = function () {
+           preview.src = reader.result;
+       }
+
+       if (file) {
+           reader.readAsDataURL(file); //reads the data as a URL
+       } else {
+           preview.src = "";
+       }
+  }
+
+
