f15b2ed47f90eeda64cd5d6059199db5f94502b3
Janeth Jepkogei <janeth.jepkogei@tufts.edu>
2016-03-13 23:13:35 -0400

    Added feature to  default to issuedisplay page on submition login information.

--- frontend/login.js
+++ frontend/login.js
@@ -1,45 +1,14 @@
 
-URL = 'http://api.dirt.frontfish.net';
-
-$( document ).ready(function() {
-	// from http://stackoverflow.com/questions/169506/obtain-form-input-fields-using-jquery
-	$( "#form" ).on( "submit", function( event ) {
-	 	event.preventDefault();
-	    var $inputs = $('#form :input');
-	    var values = {};
-	    $inputs.each(function() {
-	        if (this.name != "") {
-	        	
-            	values['User_name'] = login-username; // this will be replaced by UserID when we get one (TODO)
-            	values['password'] = ; // this will be based on the UserID when we get one (TODO)
-            	
-    			
-            	/*$.ajax({
-				  method: "POST",
-				  url: URL + '/users/new',
-				  data: values
-				})*/
-
-				$.done(function(msg) {
-				  console.log(msg);
-				  window.location = 'issuedisplay3.html'
-				});
-	        } else {
-
-	        	values[this.name] = escapeHtml($(this).val());
-	        }
-	    });
-	});
-	
-	document.getElementById('submitbutton').disabled = false;
+$( "form" ).submit(function( event ) {
+	event.preventDefault();
+	console.log("I am here"); 
+    var e = document.getElementById("login-username").value;
+   	
+   	window.location.replace("issuedisplay3.html");
 });
 
-// from http://shebang.brandonmintern.com/foolproof-html-escaping-in-javascript/
-function escapeHtml(str) {
-    var div = document.createElement('div');
-    div.appendChild(document.createTextNode(str));
-    return div.innerHTML;
-};
 
+
+ 
 
 
